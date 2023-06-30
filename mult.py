9aa79658b47a0f94d517d2b33486d705e154ff76
doranmartin <doranmartin1994@gmail.com>
2023-06-30 16:56:59 -0400

    need to revise

---
+++ mult.py
@@ -0,0 +1,19 @@
+
+# add tools for monoalphabetic ciphers :)
+
+plain = ord('h') - ord('a') + 1
+cipher = ord('P') - ord('A') + 1
+
+s = [1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25]
+
+print(plain, cipher)
+seed = 0
+
+for num in s:
+  if cipher == plain * num % 26:
+    seed = num
+    print(num)
+
+new_plain = ord('c') - ord('a') + 1
+
+print(seed * new_plain % 26)
