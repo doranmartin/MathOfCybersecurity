5693a353ad3d5dbc84cc43beaa73499c73a3355c
doranmartin <doranmartin1994@gmail.com>
2023-07-03 15:06:38 -0400

    changed gen_affine

--- gen_affine.py
+++ gen_affine.py
@@ -1,11 +1,18 @@
 # plaintext
 plaintext_string = "Do not eat gas station sushi".lower()
-plaintext = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
+plaintext_string = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
+
+ciphertext = ''
 
 # keys
 (s, r) = (3, 10)
 
 for char in plaintext_string:
     plaintext_char = ord(char) - ord('a') + 1
-    ciphertext_char = chr(ord('A') + (s * (plaintext_char + r) % 26))
-    print(ciphertext_char)
+    if char.isalpha():
+        ciphertext_char = chr(ord('A') + (s * (plaintext_char + r) % 26))
+        ciphertext += ciphertext_char
+    else:
+        ciphertext += char
+
+print(ciphertext)
