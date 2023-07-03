a455769be831bdeaeb3cc13ea08a7ff64a15e771
doranmartin <doranmartin1994@gmail.com>
2023-07-03 14:34:11 -0400

    finished multcipher

--- gen_affine.py
+++ gen_affine.py
@@ -1,8 +1,9 @@
 # plaintext
-plaintext_string = "graph"
+plaintext_string = "Do not eat gas station sushi".lower()
+plaintext = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
 
 # keys
-(s, r) = (7, 10)
+(s, r) = (3, 10)
 
 for char in plaintext_string:
     plaintext_char = ord(char) - ord('a') + 1
--- multcipher.py
+++ multcipher.py
@@ -1,9 +1,9 @@
 # Encrypts message using additive cipher
 
-plaintext = "Do not eat gas station sushi"
+plaintext = "Do not eat gas station sushi".lower()
 plaintext = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
 
-r = 11
+s = 23
 
 ciphertext = ''
 
@@ -12,9 +12,8 @@ z = ord('z')
 
 for char in plaintext:
     if char.isalpha():
-        char = ord(char) + r
-        if char >= z:
-            char = char % z + a 
+        char = s * (ord(char) - a + 1) % 26 + a - 1
+        print(char)
         ciphertext += chr(char)
     else:
         ciphertext += char
