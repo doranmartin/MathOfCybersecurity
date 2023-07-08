e6e6148dc176cef54d4606fe8bd5e034666df8af
doranmartin <doranmartin1994@gmail.com>
2023-07-08 00:03:14 -0400

    changed some things for analysis 1

--- mult_inverse.py
+++ mult_inverse.py
@@ -1,10 +1,16 @@
 # Finds x such that -> a * x == b (mod n)
 
-a = 7
-b = 1
-n = 26
+# a = 15
+# b = 1
+# n = 26
+
+# for x in range(1000):
+#     if a * x % n == b:
+#         print(x)
+#         break
+
 
 for x in range(1000):
-    if a * x % n == b:
+    if (28 * x + 40) % 26 == 18:
         print(x)
         break
