5a63ae89d31ad0a1e9765835a57904ef659b5908
doranmartin <doranmartin1994@gmail.com>
2023-07-10 10:27:58 -0400

    changed mult_inverse

--- mult_inverse.py
+++ mult_inverse.py
@@ -1,16 +1,16 @@
 # Finds x such that -> a * x == b (mod n)
 
-# a = 15
-# b = 1
-# n = 26
-
-# for x in range(1000):
-#     if a * x % n == b:
-#         print(x)
-#         break
-
+a = 21
+b = 1
+n = 26
 
 for x in range(1000):
-    if (28 * x + 40) % 26 == 18:
+    if a * x % n == b:
         print(x)
         break
+
+
+# for x in range(1000):
+#     if (28 * x + 40) % 26 == 18:
+#         print(x)
+#         break
