f46aff52dc0db3b0d4e8b393e338f30468df851e
doranmartin <doranmartin1994@gmail.com>
2023-07-10 13:59:04 -0400

    changed decrypt_hill

--- decrypt_hill.py
+++ decrypt_hill.py
@@ -1,8 +1,14 @@
-a = 17
-b = -3
+e_co = 20
+f_co = 1
+f_co_a = 10
+f_co_b = -3
 c = 5
 
+e_co += f_co * f_co_b
+f_co *= f_co_a
+    
+
 for e in range(100):
-    if (a * e + b) % 26 == c:
+    if (e_co * e + f_co) % 26 == c:
         print(e)
         break
